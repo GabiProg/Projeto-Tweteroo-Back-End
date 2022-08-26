95e7559485334d472fdc452d957a10351bcb840c
GabiProg <gabrielaterese07@gmail.com>
2022-08-26 13:50:33 -0300

    POST da rota sign-up feita

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(5000);
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.send("OK");
+});
+
+app.post("/tweets", (req, res) =>{
+
+});
+
+app.listen(5000, () => console.log("Listening port on 5000"));
