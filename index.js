fe97264caaefd04ddddadaa6231e102ead0c1b9a
GabiProg <gabrielaterese07@gmail.com>
2022-08-26 15:55:17 -0300

    POST da rota tweets feito

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body;
@@ -14,7 +15,20 @@ app.post("/sign-up", (req, res) => {
 });
 
 app.post("/tweets", (req, res) =>{
-
+    const tweet = req.body;
+    
+    const getAvatar = users.find(item => item.username === tweet.username);
+    
+    tweets.push({
+        ...tweet,
+        avatar: getAvatar.avatar
+    });
+    
+    res.send("OK");
 });
+
+// app.get("/tweets", (req, res) =>{
+//     res.send(tweets);
+// });
 
 app.listen(5000, () => console.log("Listening port on 5000"));
